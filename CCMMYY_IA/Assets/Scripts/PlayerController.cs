971db7974e7004817edad11e6bfe19b53a53a309
Luca <tbocyber@gmail.com>
2022-04-27 13:12:30 +0200

    Added 3 player

--- CCMMYY_IA/Assets/Scripts/PlayerController.cs
+++ CCMMYY_IA/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public bool canMove = false;
+    public bool onTop = false;
     public Transform movePoint;
     public LayerMask whatStopsMovement;
 
@@ -19,10 +20,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (onTop) {
+            Vector3 up = new Vector3(movePoint.position.x, movePoint.position.y, -2.0f);
+            movePoint.position = up;
+        } else {
+            Vector3 down = new Vector3(movePoint.position.x, movePoint.position.y, 0);
+            movePoint.position = down;
+        }
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f && canMove) {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement)) {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement)) {             
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
---
+++ CCMMYY_IA/Assets/Scripts/SwitchPlayer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayer : MonoBehaviour
+{
+    public GameObject[] players;
+    private int index = 0;
+    [SerializeField] public GameObject CurrentPlayer;
+ 
+    void Start()
+    {
+        for (int i = 0; i < players.Length; i++) {
+            DisableMove(players[i]);
+        }
+        CurrentPlayer = players[0];
+        EnableMove(CurrentPlayer);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            Debug.Log("Switch Player");
+            DisableMove(players[index]);
+            index++;
+            if (index > 2)
+                index = 0;
+            EnableMove(players[index]);
+        }
+        CurrentPlayer = players[index];
+    }
+
+    public void DisableMove(GameObject Player) {
+        Player.GetComponent<PlayerController>().canMove = false;
+        Player.GetComponent<PlayerController>().onTop = false;
+    }
+
+    public void EnableMove(GameObject Player) {
+        Player.GetComponent<PlayerController>().canMove = true;
+        Player.GetComponent<PlayerController>().onTop = true;
+
+    }
+}
