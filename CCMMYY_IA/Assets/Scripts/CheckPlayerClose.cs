7436b03ab8b144279a13ad8895feb722b8d0bb3e
PierpaoloLopez <lpzppl98m18d086j@studenti.unical.it>
2022-04-30 12:46:13 +0200

    creazione 3 goal+ logica arrivo 3 goal

--- CCMMYY_IA/Assets/Scripts/CheckPlayerClose.cs
+++ CCMMYY_IA/Assets/Scripts/CheckPlayerClose.cs
@@ -6,13 +6,12 @@ using UnityEngine.Tilemaps;
 public class CheckPlayerClose : MonoBehaviour
 {
     public GameObject player;
+    public GameObject goalBlock;
     public Transform movePointL;
     public Transform movePointR;
     public Transform movePointU;
     public Transform movePointD;
     public Transform goal;
-    //private bool stopPush = false;
-    //private int i = 0;
 
     private bool affiancatoLeft = false;
     private bool affiancatoRight = false;
@@ -25,6 +24,9 @@ public class CheckPlayerClose : MonoBehaviour
     public bool BlockOnWallDown = false;
 
     public Tilemap wall;
+    //private bool stopPush = false;
+    //private int i = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,8 +38,13 @@ public class CheckPlayerClose : MonoBehaviour
     {
         Vector3 posAttuale = transform.position;
         if (goal.transform.position == posAttuale) {
-            Debug.Log("Stop Hai vinto Winner");
+            //Debug.Log("Stop Hai vinto Winner");
+            goalBlock.GetComponent<GoalChekArrived>().arrived = true;
             //stopPush = true;
+        }
+        else
+        {
+            goalBlock.GetComponent<GoalChekArrived>().arrived = false;
         }
         
         Vector3 posizioneAttualePlayer = player.GetComponent<Transform>().position;
@@ -108,56 +115,4 @@ public class CheckPlayerClose : MonoBehaviour
             BlockOnWallDown = true;
     }
 
-
-    /*if(posizioneAttualePlayer == posAttualeCubo && affiancatoLeft){
-            Vector3 vec = new Vector3(transform.position.x + 1.0f, transform.position.y, transform.position.z);
-            transform.position = vec;
-            //print("collisione da sx -> dx ");
-        }
-        if(posizioneAttualePlayer == posAttualeCubo && affiancatoRight){
-            Vector3 vec = new Vector3(transform.position.x - 1.0f, transform.position.y, transform.position.z);
-            transform.position = vec;
-            //print("collisione da dx <- sx ");
-        }
-        if(posizioneAttualePlayer == posAttualeCubo && affiancatoUp){
-            Vector3 vec = new Vector3(transform.position.x, transform.position.y - 1.0f, transform.position.z);
-                transform.position = vec;
-                //print("collisione da Up \\/ giu ");
-        }
-        if(posizioneAttualePlayer == posAttualeCubo && affiancatoDown){
-            Vector3 vec = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
-                transform.position = vec;
-                //print("collisione da giu /\\ up ");
-        }*/
-
-    /*private void OnCollisionEnter2D(Collision2D collision) {
-        //if (!stopPush) {
-            Vector3 posAttualeCubo = transform.position;
-            Vector3 posizioneAttualePlayer = player.GetComponent<Transform>().position;
-            
-            
-            while (movePointL.position == posizioneAttualePlayer) {
-                Vector3 vec = new Vector3(transform.position.x + 1.0f, transform.position.y, transform.position.z);
-                transform.position = vec;
-                //print("collisione da sx -> dx ");
-            }
-            while (movePointR.position == posizioneAttualePlayer) {
-                Vector3 vec = new Vector3(transform.position.x - 1.0f, transform.position.y, transform.position.z);
-                transform.position = vec;
-                //print("collisione da dx <- sx ");
-            }
-            while (movePointU.position == posizioneAttualePlayer) {
-                Vector3 vec = new Vector3(transform.position.x, transform.position.y - 1.0f, transform.position.z);
-                transform.position = vec;
-                //print("collisione da Up \\/ giu ");
-            }
-            while (movePointD.position == posizioneAttualePlayer) {
-                Vector3 vec = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
-                transform.position = vec;
-                //print("collisione da giu /\\ up ");
-            }
-         //   print("collisione " + i);
-            i++;
-       // }
-    }*/
 }
